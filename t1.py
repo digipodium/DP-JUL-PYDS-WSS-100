0a2c0ae2e20a757f145a7f0311189a4d79514a39
zaidkamil <xaidmetamorphos@gmail.com>
2022-07-23 12:13:12 +0530

    control #1

---
+++ t1.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+forward(200) # Move forward 200 units
+left(90)     # Turn left 90 degrees
+forward(100)
+left(90)
+forward(200)
+left(90)
+forward(100)
+
+mainloop() # stops the window from closing
