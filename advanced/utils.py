539150f29e1c6162f847366aa3c5c02fa2d0fc31
zaidkamil <xaidmetamorphos@gmail.com>
2022-08-27 12:01:29 +0530

    function basics

---
+++ advanced/utils.py
@@ -0,0 +1,17 @@
+def char_counter(sentence):
+    data = {} # blank dict
+    for char in set(sentence):
+        # print(char, sentence.count(char))
+        data[char] = sentence.count(char)
+    return data
+
+def word_counter(sentence):
+    data = {}
+    words = sentence.split()
+    for word in set(words):
+        data[word] = words.count(word)
+    return data
+
+def remove_punc(sentence):
+    '''removes punctuation ( TBC )'''
+    pass
